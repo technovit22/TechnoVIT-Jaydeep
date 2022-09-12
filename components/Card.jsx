2006c0c0cf2cd13a2439d82f5c68be7598faba01
Jaydeep <jaydeep.dey03@gmail.com>
2022-09-12 11:42:24 +0530

    added dailyevents

---
+++ components/Card.jsx
@@ -0,0 +1,11 @@
+import { motion } from "framer-motion";
+
+const Card = () => {
+    return (
+        <div className="bg-blue-400 h-[25rem] rounded w-[20rem]">
+            
+        </div>
+    )
+}
+
+export default Card
--- components/Dailyevents.jsx
+++ components/Dailyevents.jsx
@@ -1,4 +1,10 @@
 import { AnimatePresence, motion } from "framer-motion"
+import Card from "./Card"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
 
 const eventVariant = {
     hidden: {
@@ -9,20 +15,54 @@ const eventVariant = {
         opacity: 1,
         scale: 0.8,
         transition: {
-            type: "spring",
-            delay: 0.3,
-        },
+            delay: 0.5,
+            when: 'beforeChildren',
+            staggerChildren: 0.2,
+        }
     },
+}
+
+const listVariant = {
+    hidden: {
+        scale: 0,
+        opacity: 0,
+    },
+    visible: {
+        scale: 1,
+        opacity: 1,
+    }
 }
 
 const Dailyevents = () => {
     return (
         <div>
-            <div className="theme">
+            <div className="theme3">
                 <AnimatePresence>
                     <motion.div variants={eventVariant} whileInView={"visible"} initial={"hidden"} className="h-screen w-screen font-poppins text-6xl text-center p-10">
-                        <motion.h1 className="font-bold text-white">Daily Events</motion.h1>
-                        {/* <motion.img src={`/envsus.png`} className="z-50 h-[12rem] m-auto mt-10" alt="" /> */}
+                        <motion.h1 variants={listVariant} className="font-bold text-white mb-10">Daily Events</motion.h1>
+                        {/* <div className="p-10 flex space-x-7"> */}
+                        <motion.div variants={listVariant} className="p-16 swiper-class">
+                            <Swiper
+                                slidesPerView={3}
+                                spaceBetween={30}
+                                slidesPerGroup={3}
+                                loop={true}
+                                loopFillGroupWithBlank={false}
+                                pagination={{
+                                    clickable: true,
+                                }}
+                                navigation={true}
+                                modules={[Pagination, Navigation]}
+                                className="mySwiper p-10 flex space-x-7"
+                            >
+                                <SwiperSlide><Card /></SwiperSlide>
+                                <SwiperSlide><Card /></SwiperSlide>
+                                <SwiperSlide><Card /></SwiperSlide>
+                                <SwiperSlide><Card /></SwiperSlide>
+                                <SwiperSlide><Card /></SwiperSlide>
+                            </Swiper>
+                        </motion.div>
+                        {/* </div> */}
                     </motion.div>
                 </AnimatePresence>
             </div>
