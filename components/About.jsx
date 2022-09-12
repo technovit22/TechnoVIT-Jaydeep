8a73b6a2c2893241b9f90813233299d3835c7a7a
Jaydeep <jaydeep.dey03@gmail.com>
2022-09-12 10:52:36 +0530

    minor changes

--- components/About.jsx
+++ components/About.jsx
@@ -26,7 +26,7 @@ const eventVariant = {
 
 const gateVariant = {
     hidden: {
-       scale: 0,
+        scale: 0,
     },
     visible: {
         scale: 1,
@@ -39,46 +39,58 @@ const gateVariant = {
 
 const contentVariant = {
     hidden: {
-        x: '100vw',
+        x: '-100vw',
     },
     visible: {
         x: 0,
         transition: {
-            type: "spring",
-            delay: 1.2,
-        },
-    },
+            delay: 0.1,
+            when: 'beforeChildren',
+            staggerChildren: 0.4,
+        }
+    }
 }
 
 const listVariant = {
     hidden: {
-        x: -10,
+        x: '100vw',
         opacity: 0,
     },
     visible: {
         x: 0,
         opacity: 1,
+        transition: {
+            damping: '30'
+        }
     }
+
 }
 
 const About = () => {
     return (
         <div className="theme1">
             <AnimatePresence>
-                <motion.div variants={eventVariant} whileInView={"visible"} initial={"hidden"} className="h-screen w-screen">
-                    <motion.h1 initial={"hidden"} animate={"visible1"} className="font-bold text-white text-6xl text-center font-poppins p-10 pb-3">About TechnoVIT
-                    </motion.h1>
+                <div className="h-screen w-screen">
+                    <div className="flex space-x-10 p-16 items-center">
 
-                    <motion.div variants={contentVariant} initial={"initial"} animate={"visible"} className="flex space-x-10 p-16">
-                        <motion.div variants={gateVariant} initial={"hidden"} whileInView={"visible"} className="w-[50vw] h-[25rem]">
+                        <motion.div variants={gateVariant} initial={"hidden"} whileInView={"visible"} className="w-[50vw] h-[25rem] flex justify-center items-center">
                             <img src={`/vitgate.png`} alt="" />
                         </motion.div>
-                        <motion.div variants={listVariant} initial={"initial"} whileInView={"visible1"} className="w-[50vw] h-[25rem] font-nanum text-white font-semibold tracking-wide text-xl">
-                            This is the 6th Edition of technoVIT, an international level technical festival of VIT Chennai.It gives us immense pride and pleasure in presenting technoVIT' 22. This technical extravaganza will be filled with numerous activities such as workshops and technical events organised by the various schools of the University with the primary objective being, " nurture professional skills and impart fine qualities to the students". It also has gaming events to thrill and create an environment with fun and frolic. Additionally, technical exhibitions from various organisations are being hosted. We, at VIT Chennai promise the audience an atmosphere of immense joy leaving them the thirst for more.
-                        </motion.div>
-                    </motion.div>
 
-                </motion.div>
+
+                        <motion.div variants={contentVariant} initial={"hidden"}
+                        animate={"visible"} className="flex flex-col space-y-6">
+                            <motion.h1 variants={listVariant} className="font-bold text-white text-6xl font-poppins pt-10 pb-3">About TechnoVIT
+                            </motion.h1>
+                            <motion.div variants={listVariant} className="w-[50vw] font-nanum text-white font-semibold tracking-wide text-xl">
+                                This is the 6th Edition of technoVIT, an international level technical festival of VIT Chennai.It gives us immense pride and pleasure in presenting technoVIT' 22. This technical extravaganza will be filled with numerous activities such as workshops and technical events organised by the various schools of the University with the primary objective being, " nurture professional skills and impart fine qualities to the students". It also has gaming events to thrill and create an environment with fun and frolic.
+                            </motion.div>
+                            <motion.div variants={listVariant} className="w-[50vw] font-nanum text-white font-semibold tracking-wide text-xl">
+                                Additionally, technical exhibitions from various organisations are being hosted. We, at VIT Chennai promise the audience an atmosphere of immense joy leaving them the thirst for more.
+                            </motion.div>
+                        </motion.div>
+                    </div>
+                </div>
             </AnimatePresence>
         </div>
     )
