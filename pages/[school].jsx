22e750d68c7602dcc33bff94382bc44a84f6a014
Jaydeep <jaydeep.dey03@gmail.com>
2022-09-13 09:18:55 +0530

    minor changes

---
+++ pages/[school].jsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+import Router from "next/router";
+
+
+const School = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default School
